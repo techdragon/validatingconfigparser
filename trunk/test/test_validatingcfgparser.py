3e955b7d8bb0a45f5394949b1e399d3138bc45d9
markus.at.juenemann@gmail.com <markus.at.juenemann@gmail.com@5613416c-d4ab-0d6b-2203-592a373d569a>
2012-10-15 12:44:17 +0000

    Added tests for schema and items()

--- trunk/test/test_validatingcfgparser.py
+++ trunk/test/test_validatingcfgparser.py
@@ -1,21 +1,33 @@
 
 
 
-
+from types import *
 
 import validatingconfigparser
-import formencode.validators
+from formencode.validators import *
+from formencode import *
 import formencode.schema
 
 from nose.tools import *
 
 
 class Schema(formencode.schema.Schema):
-    integer = formencode.validators.Int()
-#    stringbool_true = formencode.validators.StringBool()
-#    stringbool_false = formencode.validators.StringBool()
-    bool_true = formencode.validators.String()
-    bool_false = formencode.validators.String()
+    integer_good = Int()
+    integer_too_small = Int(min=1)
+    integer_too_big = Int(max=10)
+    bool_zero = Bool()
+    bool_one = Bool()
+    stringbool_true = StringBool()
+    stringbool_false = StringBool()
+    stringbool_yes = StringBool()
+    stringbool_no = StringBool()
+    number_int_good = Number()
+    number_int_too_small = Number(min=5)
+    number_int_too_big = Number(max=50)
+    number_float_good = Number()
+    number_float_too_small = Number(min=5.0)
+    number_float_too_big = Number(max=50.0)
+
 
 
 class RawConfigParserWithoutSchema():
@@ -52,72 +64,146 @@ class SafeConfigParserWithSchema():
 class IntTests():
     def test_get_Int(self):
         if self.parser.schema is None:
-            assert self.parser.get("firstsection", "integer", formencode.validators.Int()) == 1
+            validator = Int()
+            assert self.parser.get("firstsection", "integer_good", validator) == 1
         else:
-            assert self.parser.get("firstsection", "integer") == 1
+            assert self.parser.get("firstsection", "integer_good") == 1
         
-    def test_get_Int_within_min_max(self):
-        validator = formencode.validators.Int(min=1, max=1)
-        assert self.parser.get("firstsection", "integer", validator) == 1
+    @raises(Invalid)
+    def test_get_Int_too_small(self):
+        if self.parser.schema is None:
+            validator = Int(min=1)
+            self.parser.get("firstsection", "integer_too_small", validator)
+        else:
+            self.parser.get("firstsection", "integer_too_small")
+        
+    @raises(Invalid)
+    def test_get_Int_too_big(self):
+        if self.parser.schema is None:
+            validator = Int(max=10)
+            self.parser.get("firstsection", "integer_too_big", validator) == 1
+        else:
+            self.parser.get("firstsection", "integer_too_big") == 1
     
-    @raises(formencode.Invalid)
-    def test_get_Int_outside_min_max(self):
-        validator = formencode.validators.Int(min=2, max=2)
-        assert self.parser.get("firstsection", "integer", validator) == 1
-
-
-#    def test_getint_Int(self):
-#        validator = formencode.validators.Int()
-#        assert self.parser.getint("firstsection", "integer", validator) == 1
-#        
-#    def test_getint_Int_within_min_max(self):
-#        validator = formencode.validators.Int(min=1, max=1)
-#        assert self.parser.getint("firstsection", "integer", validator) == 1
-#    
-#    @raises(formencode.Invalid)
-#    def test_getint_Int_outside_min_max(self):
-#        validator = formencode.validators.Int(min=2, max=2)
-#        assert self.parser.getint("firstsection", "integer", validator) == 1
-
-
 class BoolTests():
-    def test_get_StringBool_false(self):
-        validator = formencode.validators.StringBool()
-        assert self.parser.get("firstsection", "stringbool_false", validator) is False
-    
+    def test_get_Bool_zero(self):
+        # The string '0' is interpreted as True!
+        if self.parser.schema is None:
+            validator = Bool()
+            assert self.parser.get("firstsection", "bool_zero", validator) is True
+        else:
+            assert self.parser.get("firstsection", "bool_zero") is True
+
+    def test_get_Bool_one(self):
+        if self.parser.schema is None:
+            validator = Bool()
+            assert self.parser.get("firstsection", "bool_one", validator) is True
+        else:
+            assert self.parser.get("firstsection", "bool_one") is True
+
+class StringBoolTests():
     def test_get_StringBool_true(self):
-        validator = formencode.validators.StringBool()
-        assert self.parser.get("firstsection", "stringbool_true", validator) is True
-        
-        
-#    def test_getboolean_StringBool_false(self):
-#        validator = formencode.validators.StringBool()
-#        assert self.parser.getboolean("firstsection", "stringbool_false", validator) is False
-#    
-#    def test_getboolean_StringBool_true(self):
-#        validator = formencode.validators.StringBool()
-#        assert self.parser.getboolean("firstsection", "stringbool_true", validator) is True
-        
-        
-#    def test_get_Bool_false(self):
-#        validator = formencode.validators.Bool()
-#        assert self.parser.get("firstsection", "bool_false", validator) is False
+        if self.parser.schema is None:
+            validator = StringBool()
+            assert self.parser.get("firstsection", "stringbool_true", validator) is True
+        else:
+            assert self.parser.get("firstsection", "stringbool_true") is True
+
+    def test_get_StringBool_false(self):
+        if self.parser.schema is None:
+            validator = StringBool()
+            assert self.parser.get("firstsection", "stringbool_false", validator) is False
+        else:
+            assert self.parser.get("firstsection", "stringbool_false") is False
+
+    def test_get_StringBool_yes(self):
+        if self.parser.schema is None:
+            validator = StringBool()
+            assert self.parser.get("firstsection", "stringbool_yes", validator) is True
+        else:
+            assert self.parser.get("firstsection", "stringbool_yes") is True
+
+    def test_get_StringBool_no(self):
+        if self.parser.schema is None:
+            validator = StringBool()
+            assert self.parser.get("firstsection", "stringbool_no", validator) is False
+        else:
+            assert self.parser.get("firstsection", "stringbool_no") is False
+
+class NumberTests():
+    def test_number_int_good(self):
+        if self.parser.schema is None:
+            validator = Number()
+            assert self.parser.get("firstsection", "number_int_good", validator) is 10
+        else:
+            assert self.parser.get("firstsection", "number_int_good") is 10
+
+    @raises(Invalid)
+    def test_number_int_too_small(self):
+        if self.parser.schema is None:
+            validator = Number(min=5)
+            self.parser.get("firstsection", "number_int_too_small", validator)
+        else:
+            self.parser.get("firstsection", "number_int_too_small")
+
+    @raises(Invalid)
+    def test_number_int_too_big(self):
+        if self.parser.schema is None:
+            validator = Number(max=50)
+            self.parser.get("firstsection", "number_int_too_big", validator)
+        else:
+            self.parser.get("firstsection", "number_int_too_big")
+
+    def test_number_float_good(self):
+        if self.parser.schema is None:
+            validator = Number()
+            assert self.parser.get("firstsection", "number_float_good", validator) == 10.5
+        else:
+            assert self.parser.get("firstsection", "number_float_good") == 10.5
+
+    @raises(Invalid)
+    def test_number_float_too_small(self):
+        if self.parser.schema is None:
+            validator = Number(min=5.0)
+            self.parser.get("firstsection", "number_float_too_small", validator)
+        else:
+            self.parser.get("firstsection", "number_float_too_small")
+
+    @raises(Invalid)
+    def test_number_float_too_big(self):
+        if self.parser.schema is None:
+            validator = Number(max=50.0)
+            self.parser.get("firstsection", "number_float_too_big", validator)
+        else:
+            self.parser.get("firstsection", "number_float_too_big")
+
+class SchemaTests():
+    def test_no_validator(self):
+        if self.parser.schema is None:
+            assert self.parser.get("firstsection", "no_validator") == "10"
+        else:
+            self.parser.get("firstsection", "no_validator") == "10"
+
+    def test_items(self):
+        items = self.parser.items("good_section")
+        if self.parser.schema is None:
+            # Without schema, all values are returned as strings.
+            for (option, value) in items:
+                assert isinstance(value, StringType)
+        else:
+            assert items[0][1] == 1		# integer_good
+            assert items[1][1] is True		# bool_one
+            assert items[2][1] is True		# bool_zero, '0' == True
+            assert items[3][1] is True		# stringbool_true
+            assert items[4][1] is False		# stringbool_false
+            assert items[5][1] is True		# stringbool_yes
+            assert items[6][1] is False		# stringbool_no
+            assert items[7][1] == 10		# number_int_good
+            assert items[8][1] == 10.5		# number_float_good
+            assert items[9][1] == "10"		# no_validator
     
-    def test_get_Bool_true(self):
-        validator = formencode.validators.Bool()
-        assert self.parser.get("firstsection", "bool_true", validator) is True
-        
-        
-#    def test_getboolean_Bool_false(self):
-#        validator = formencode.validators.Bool()
-#        assert self.parser.getboolean("firstsection", "bool_false", validator) is False
-    
-#    def test_getboolean_Bool_true(self):
-#        validator = formencode.validators.StringBool()
-#        assert self.parser.getboolean("firstsection", "bool_true", validator) is True
-#    
-    
-class AllTests(IntTests, BoolTests): pass  
+class AllTests(IntTests, BoolTests, StringBoolTests, NumberTests, SchemaTests): 
+    pass  
         
 class TestRawConfigParserWithoutSchema(RawConfigParserWithoutSchema, AllTests): pass                
 
--- trunk/validatingconfigparser/validatingconfigparser.py
+++ trunk/validatingconfigparser/validatingconfigparser.py
@@ -11,24 +11,30 @@
       keyword argument.
       
       >>> value = parser.get(section, option, validator=my_validator)
+      >>> parser.set(section, option, value, validator=my_validator)
+
+      If no `validator` argument is provided, then either a matching validator 
+      of the schema will be used or `value` will be used as is.
       
     - The `items()` method of above classes will observe the validation schema
-      that was passed to the class when it was instantiated.
+      of the parser.
+
+      >>> parser.schema = my_other_schema
+      >>> items = parser.items()
       
-    - All other methods are remain unchanged. The `getint()`, `getfloat()`
+    - All other methods remain unchanged. The `getint()`, `getfloat()`
       and `getboolean()` methods do not provide validation beyond their
-      original logic.  
+      original logic of coercing the value into an integer, float or
+      boolean type. If validation of such values is required one has to 
+      use the `get()` method with a suitable validator instead, e.g.
+
+      >>> from formencode.validators import Int, 
+      >>> parser.get(section, option, validator=Int())
       
     Schemas and validators must follow the same API as their counterparts
-    of the **Formencode** package. The `validatingconfigparser` package
-    was explicitely designed so that the classes provied by **Formencode**
+    of the `Formencode` package. The `validatingconfigparser` package
+    was explicitely designed so that the classes provided by `Formencode`
     can be used as is.     
-
-.. warning:: 
-
-    The classes in the `ConfigParser` module of the Python Standard library are
-    old-style classes (http://bugs.python.org/issue5807), i.e. they don't inherit
-    from `object`. In contrast the classes in this module are new style classes.
    
 """
 
@@ -37,108 +43,42 @@ __license__ = "Apache License 2.0"
 __revision__ = "$Rev:$"
 
 
-# Import everything from ConfigParser into the local namespace, but
-# ensure that the ConfigParser, SafeConfigParser and RawConfigParser
+# Import the parser classes from ConfigParser into the local namespace, 
+# but ensure that the ConfigParser, SafeConfigParser and RawConfigParser
 # classes are available under a different name as they are being
 # overwritten by this module.
 #
-from ConfigParser import *
 from ConfigParser import ConfigParser as _ConfigParser
 from ConfigParser import SafeConfigParser as _SafeConfigParser
 from ConfigParser import RawConfigParser as _RawConfigParser
-#from ConfigParser import _default_dict
 
-import formencode.validators
-import formencode.schema
+# The Validator base class always returns the original value unchanged.
+# This makes it useful as a "dummy" default validator.
+#
+from formencode.api import Validator
 
-
-#class DummySchema(formencode.schema.Schema):
-#    def __init__(self, *args, **kwargs):
-#        super(DummySchema, self).__init__(allow_extra_fields=True, *args, **kwargs)
-
-
-class DummyValidator(formencode.validators.FancyValidator):
-    """
-    Base class for derived validators.
-
-    TODO: Rewrite this __doc__
-    
-    `Validator` is loosely modelled after the `formencode.api.FancyValidator` class.
-    It provides two methods, `to_python()` and `from_python()`. The `to_python()`
-    method is called by the `get\*()` methods of a configuration parser to convert and validate 
-    a value before it is returned to the caller. The `from_python()` method is called by the 
-    `set()` method to validate and convert a value before it is written to a configuration file. 
-    Logically this looks like this:
-    
-    ::
-    
-      to_python(get(...))  
-      to_python(getint(...))
-      to_python(getfloat(...))
-      to_python(getboolean(...))
-      
-      set(..., from_python(value))
-      
-    The `Validator` base class does actually not perform any validation at all. It is expected that
-    any derived classes override the `_to_python()` and `_from_python()` (note the leading
-    underscore!) methods and perform any validations within these methods. If validations is 
-    successful `_to_python()` and `_from_python()` must return the validated value. Both methods
-    may convert or format the value as they deem necessary. Care must be taken that the value
-    returned by `_from_python()` can be converted to a string so that it can be written to the
-    configuration file.  
-    
-    In contrast to `formencode.api.FancyValidator` the `Validator` class of this module
-    does neither provide `validate_python()` and `validate_others()` methods nor does its
-    `__init__()` method accept any arguments.
-    
-    """
-    
-    def _to_python(self, value, state):
-        """
-        Called by `to_python()` to validate and convert `value`.
-        
-        :param value: The value to be validated and converted.
-        
-        `_to_python` should be overwritten by derived classes as the default
-        simply returns the original `value`.
-        
-        """
-
-        return value
-        
-    def _from_python(self, value, state):
-        """
-        Called by `from_python()` to validate and convert `value`.
-        
-        :param value: The value to be validated and converted.
-        
-        `_from_python` should be overwritten by derived classes as the default
-        simply returns the original `value`.
-        
-        """
-        
-        return value
-    
 
 class ValidatingMixIn(object):
+    """
+    Provides the validating variants of `get()`, `set()` and `items()` methods.
+
+    """
+
     def __init__(self, *args, **kwargs):
         try:
             self.schema = kwargs.pop("schema")
         except KeyError:
-            self.schema = None  #DummySchema()
+            self.schema = None  
 
-        """
-        Find my parent class. This is one of
-        * _RawConfigParser
-        * _ConfigParser
-        * _SafeConfigParser
-        
-        .. important:: The code below must be separate ``if`` statements
-            and not an ``if/elif`` construct. Also the ``if`` statements
-            must appear in the exact order.
+        # Find my parent class. This is one of
+        # * _RawConfigParser
+        # * _ConfigParser
+        # * _SafeConfigParser
+        # 
+        # IMPORTANT The code below must be separate ``if`` statements
+        #    and not an ``if/elif`` construct. Also the ``if`` statements
+        #    must appear in the exact order. Don't change the code below.
             
-        """            
-        
         if isinstance(self, _RawConfigParser):
             self.parent = _RawConfigParser
         
@@ -149,6 +89,7 @@ class ValidatingMixIn(object):
             self.parent = _SafeConfigParser
             
         self.parent.__init__(self, *args, **kwargs)
+
 
     def find_validator(self, section, option):
         """
@@ -163,7 +104,7 @@ class ValidatingMixIn(object):
         try:
             return self.schema.fields[option]
         except (KeyError, AttributeError):
-            return DummyValidator()
+            return Validator()
         
         
     def get(self, section, option, validator=None, *args, **kwargs):
@@ -175,7 +116,10 @@ class ValidatingMixIn(object):
                 
         With the exception of `validator` all arguments (`section`, `option`, `*args` and
         `**kwargs`) will be passed on to the parent's `get()` method. The parent method 
-        will raise ``TypeError`` if it receives an argument that it does not support.    
+        will raise exception if it receives an argument that it does not support.    
+
+        Validation will be performed on the string value returned by the parent's `get()`
+        method.
         
         """
         
@@ -184,75 +128,6 @@ class ValidatingMixIn(object):
           
         return validator.to_python(self.parent.get(self, section, option, 
                                                    *args, **kwargs))
-    
-    
-#    def getint(self, section, option, validator=None, *args, **kwargs):
-#        """
-#        Add `validator` keyword argument to parent's `getint()` method.
-#        
-#        Parent will be one of `ConfigParser.RawConfigParser`, `ConfigParser.ConfigParser`
-#        or ``ConfigParser.SafeConfigParser`.  
-#                
-#        With the exception of `validator` all arguments (`section`, `option`, `*args` and
-#        `**kwargs`) will be passed on to the parent's `getint()` method. The parent method 
-#        will raise ``TypeError`` if it receives an argument that it does not support.  
-#        
-#        Validation will be performed on the value that is returned by the parent's
-#        `getint()` method.   
-#        
-#        """
-#        
-#        if validator is None:
-#            validator = self.find_validator(section, option)
-#        
-#        return validator.to_python(self.parent.getint(self, section, option, 
-#                                                      *args, **kwargs))
-#    
-#    
-#    def getfloat(self, section, option, validator=None, *args, **kwargs):
-#        """
-#        Add `validator` keyword argument to parent's `getfloat()` method.
-#        
-#        Parent will be one of `ConfigParser.RawConfigParser`, `ConfigParser.ConfigParser`
-#        or ``ConfigParser.SafeConfigParser`.  
-#                
-#        With the exception of `validator` all arguments (`section`, `option`, `*args` and
-#        `**kwargs`) will be passed on to the parent's `getfloat()` method. The parent method 
-#        will raise ``TypeError`` if it receives an argument that it does not support.
-#        
-#        Validation will be performed on the value that is returned by the parent's
-#        `getfloat()` method.   
-#        
-#        """
-#        
-#        if validator is None:
-#            validator = self.find_validator(section, option)
-#        
-#        return validator.to_python(self.parent.getfloat(self, section, option, 
-#                                                        *args, **kwargs))
-#    
-#    
-#    def getboolean(self, section, option, validator=None, *args, **kwargs):
-#        """
-#        Add `validator` keyword argument to parent's `getboolean()` method.
-#        
-#        Parent will be one of `ConfigParser.RawConfigParser`, `ConfigParser.ConfigParser`
-#        or ``ConfigParser.SafeConfigParser`.  
-#                
-#        With the exception of `validator` all arguments (`section`, `option`, `*args` and
-#        `**kwargs`) will be passed on to the parent's `getboolean()` method. The parent method 
-#        will raise ``TypeError`` if it receives an argument that it does not support.
-#        
-#        Validation will be performed on the value that is returned by the parent's
-#        `getboolean()` method.   
-#        
-#        """
-#        
-#        if validator is None:
-#            validator = self.find_validator(section, option)
-#        
-#        return validator.to_python(self.parent.getboolean(self, section, option, 
-#                                                          *args, **kwargs))
     
 
     def set(self, section, option, value, validator=None, *args, **kwargs):
@@ -264,18 +139,14 @@ class ValidatingMixIn(object):
                 
         With the exception of `validator` all arguments (`section`, `option`, `*args` and
         `**kwargs`) will be passed on to the parent's `getboolean()` method. The parent method 
-        will raise ``TypeError`` if it receives an argument that it does not support.
+        will raise an exception if it receives an argument that it does not support.
         
         Validation will be performed *before* the parent's `set()` method is being called.  
         
         """
         
         if validator is None:
-            # Find a matching validator for this `option` in `self.schema` or
-            # return the default validator which simply returns the `value`
-            # unchanged.
-            #
-            validator = getattr(self.schema, option, DummyValidator())
+            validator = self.find_validator(section, option)
         
         self.parent.set(self, section, option, validator.from_python(value), 
                         *args, **kwargs)
@@ -286,7 +157,9 @@ class ValidatingMixIn(object):
         Extend parent's `items()` method to validate every returned option against a schema.
         
         Schema refers to the value of the `schema` attribute which can be given as an argument
-        to the `__init__()` method or set later. If no schema is available 
+        to the `__init__()` method or set later. If no schema is available the items will be
+        returned as is. The same applies to individual values if there is no matching
+        validator in the schema.
          
         """
         
@@ -295,22 +168,20 @@ class ValidatingMixIn(object):
         if self.schema is None:
             return items
         else:
-            # TODO: change items dictionary inline instad of copying
-            #       all entries to validated_items.
-            validated_items = dict()
-            
+            validated_items = []
             for (option, value) in items:
                 
                 # Find a matching validator for this `option` in `self.schema` 
                 # or return the default validator which simply returns the 
                 # `value` unchanged.
                 #
-                validator = getattr(self.schema, option, DummyValidator())
+                validator = self.find_validator(section, option)
                 
                 # Validate the `value` and add it to the dictionary of validated
                 # items.
                 #
-                validated_items[option] = validator.to_python(value)
+                validated_value = validator.to_python(value)
+                validated_items.append((option, validated_value))
                 
             return validated_items
                  
